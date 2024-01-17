f6a7648d9c8c7939249f714fda3e3376fa514203
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-17 05:50:41 +0530

    Wed Jan 17 05:50:41 AM IST 2024

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,7 @@ end
 
 config.color_scheme = 'carbonfox'
 config.font = wezterm.font ('JetBrains Mono Nerd Font')
-config.font_size = 18
+config.font_size = 16
 config.window_padding = {
     left = 10,
     right = 10,
