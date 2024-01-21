0ab015232bd4e6da17f399d315c14fcc3aff9736
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-21 12:18:49 +0530

    Sun Jan 21 12:18:49 PM IST 2024

--- nvim/init.lua
+++ nvim/init.lua
@@ -125,8 +125,8 @@ require("nvim-treesitter.configs").setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
 --lsp
 require("mason").setup({
