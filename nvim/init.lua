2b5befc22107d83b5e407c8ec8b36abc95b80ba0
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-22 14:26:13 +0530

    Mon Jan 22 02:26:13 PM IST 2024

--- nvim/init.lua
+++ nvim/init.lua
@@ -290,5 +290,6 @@ vim.keymap.set('n', '<C-f>', '<C-f>zz');
 vim.keymap.set('n', '<C-b>', '<C-b>zz');
 vim.keymap.set('n', '<leader>m', ':Mason<CR>');
 vim.keymap.set('n', '<leader>la', ':Lazy<CR>');
+vim.keymap.set('n', '<leader>al', 'GVgg=G');
 
 
