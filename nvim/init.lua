09aaa425de0fd432a79529205f97552997101bc5
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-01-22 15:34:01 +0530

    Mon Jan 22 03:34:01 PM IST 2024

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,7 +24,7 @@ vim.o.scrolloff        = 10
 vim.g.vimwiki_list = {{
     syntax = "markdown",
     ext = ".md"
-  }}
+}}
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -116,6 +116,16 @@ local plugins = {
         "vimwiki/vimwiki",
     },
     ---------------------------------------------------------------------
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
     ---------------------------------------------------------------------
     ---------------------------------------------------------------------
     ---------------------------------------------------------------------
