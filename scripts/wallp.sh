4b079f421aa3587db7644c8b9dce42c96b23034b
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-03-08 00:39:51 +0530

    Fri Mar  8 12:39:51 AM IST 2024

--- scripts/wallp.sh
+++ scripts/wallp.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 
 wallpaper=$1
-swaybg -i "$wallpaper" -m fill &
 wal -i "$wallpaper" 
 killall waybar
+swaybg -i "$wallpaper" -m fill &
 sleep 0.5
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
+waybar &
