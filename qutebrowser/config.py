e0be69bd1a594b076247e3514c1de8552b05438d
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-03-09 17:59:43 +0530

    Sat Mar  9 05:59:42 PM IST 2024

---
+++ qutebrowser/config.py
@@ -0,0 +1,12 @@
+import pywalQute.draw
+
+config.load_autoconfig()
+
+pywalQute.draw.color(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+c.fonts.default_family = ["JetBrainsMono Nerd Font"]
