c0789d1b8022d2f26e1213026b0d961ca5560686
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-03-09 17:59:13 +0530

    Sat Mar  9 05:59:13 PM IST 2024

--- scripts/namewall.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-start=1
-for filename in *;
-do
-    mv "$filename" "$start.png"
-    ((start++))
-done
--- scripts/wallp.sh
+++ scripts/wallp.sh
@@ -3,6 +3,8 @@
 wallpaper=$1
 wal -i "$wallpaper" 
 killall waybar
+killall swaync
 swaybg -i "$wallpaper" -m fill &
 sleep 0.5
 waybar &
+swaync &
